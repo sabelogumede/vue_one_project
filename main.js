f99c5a399df7d968a09018e2b35c1c7395985ff9
sabelogumede <iamsabelogumede@gmail.com>
2019-02-05 14:46:42 +0200

    computed properties

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var app = new Vue({
     el: '#app',
-
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        inStock: false,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
+
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -24,8 +26,20 @@ var app = new Vue({
         addToCart: function() {
             this.cart += 1
         },
-        updateProduct: function(variantImage) {
-            this.image = variantImage
+        updateProduct: function(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
