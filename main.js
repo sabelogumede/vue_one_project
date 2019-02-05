ec125a40a10e7c3734821ac727fc8ffe98c73ed2
sabelogumede <iamsabelogumede@gmail.com>
2019-02-05 12:19:51 +0200

    vue condition...

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+
+    data: {
+        product: 'Socks',
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+    }
+})
